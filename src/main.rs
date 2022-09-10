68ba01892cf32640d72898509075fc939a8d79e8
Jiayu Hu <jiayu.hu@intel.com>
2022-09-10 10:33:04 -0400

    UnsafeCell: update mut by immut self

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::sync::atomic::{AtomicUsize, Ordering};
+use std::{sync::atomic::{AtomicUsize, Ordering}, cell::UnsafeCell};
 
 struct Simple {
     count: AtomicUsize
@@ -11,6 +11,25 @@ impl Simple {
     }
 }
 
+struct Data {
+    d: UnsafeCell<usize>
+}
+
+impl Data {
+    fn new(t: usize) -> Self {
+        Data {
+            d: UnsafeCell::new(t)
+        }
+    }
+
+    fn update(&self) {
+        let x = self.d.get();
+        unsafe {
+            (*x) += 1;
+        }
+    }
+}
+
 fn main() {
     let s = Simple {
         count: AtomicUsize::new(0)
@@ -18,4 +37,15 @@ fn main() {
 
     s.update();
     s.update();
+
+    let mut data = Data::new(0);
+    println!("Init Data = {}", data.d.get_mut());
+
+    // For UnsafeCell, borrowing check is during runtime, rather than at compile stage.
+    // If it checks at compile stage, rd1 and rd2 cannot exist at the same time.
+    let rd1 = &mut data;
+    rd1.update();
+    let rd2 = &mut data;
+    rd2.update();
+    println!("After Data = {}", data.d.get_mut());
 }
